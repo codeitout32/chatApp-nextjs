25956adfc5fe83f7ea7909598e3be7685b8bfdab
Homendra chandrawanshi <cjgoku.homes@gmail.com>
2025-01-21 16:41:06 +0530

    env changes done for deployement

--- server-sql/src/config/constants.ts
+++ server-sql/src/config/constants.ts
@@ -1,3 +1,15 @@
+import dotEnv from "dotenv"
 
-export const SECRET_KEY = 'someSecret'
-export const CLIENT_URL = 'http://localhost:3000/'
+
+dotEnv.config();
+
+export const SECRET_KEY = process.env.SECRET_KEY
+export const CLIENT_URL = process.env.CLIENT_URL
+export const PORT = process.env.PORT 
+export const POSTGRES = {
+    HOST: process.env.PG_HOST,
+    PORT:   process.env.PG_PORT,
+    USERNAME: process.env.PG_USERNAME,
+    PASSWORD: process.env.PG_PASSWORD,
+    DATABASE: process.env.PG_DATABASE,
+}
--- server-sql/src/data-source.ts
+++ server-sql/src/data-source.ts
@@ -4,14 +4,16 @@ import { User } from "./entity/User"
 import { Photo } from "./entity/Photo"
 import { Message } from "./entity/Message"
 import { Chat } from "./entity/Chat"
+import { POSTGRES } from "./config/constants"
+
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "admin",
-    database: "postgres",
+    host: POSTGRES.HOST,
+    port: POSTGRES.PORT,
+    username: POSTGRES.USERNAME,
+    password: POSTGRES.PASSWORD,
+    database: POSTGRES.DATABASE,
     synchronize: true,
     logging: false,
     entities: [User, Photo,Message,Chat],
