38f347c718992a14c274f476e71dae3de62c7a30
Homendra chandrawanshi <cjgoku.homes@gmail.com>
2025-02-09 18:32:29 +0530

    try 8

--- server-sql/src/config/socket.ts
+++ server-sql/src/config/socket.ts
@@ -53,6 +53,8 @@ const initializeSocket = (server: any) => {
         })
 
         socket.on("connection_error", (err) => {
+            console.log('socket connecton error');
+            
             console.log(err.req);      // the request object
             console.log(err.code);     // the error code, for example 1
             console.log(err.message);  // the error message, for example "Session ID unknown"
